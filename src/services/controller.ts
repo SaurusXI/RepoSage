91c31c7331b3662b0b1059c21c5a1d3d0046a533
Shantanu Verma <vermashantanu@hotmail.com>
2023-08-18 10:13:14 +0530

    feat: add incident analysis tool

--- src/services/controller.ts
+++ src/services/controller.ts
@@ -1,5 +1,6 @@
 import DiffsKnowledgeRepository from './knowledge/diffs.js';
 import ChangelogTool from './tools/changelog.js';
+import IncidentTool from './tools/incident.js';
 import SummaryTool from './tools/summary.js';
 import TestCasesTool from './tools/tc.js';
 
@@ -12,11 +13,14 @@ export default class ControllerAgent {
 
     private changelogTool: ChangelogTool;
 
+    private incidentTool: IncidentTool;
+
     constructor() {
         this.diffsRepo = new DiffsKnowledgeRepository();
         this.testCasesTool = new TestCasesTool(this.diffsRepo);
         this.summaryTool = new SummaryTool(this.diffsRepo);
         this.changelogTool = new ChangelogTool(this.diffsRepo);
+        this.incidentTool = new IncidentTool(this.diffsRepo);
     }
 
     async processPR(prURL: string) {
@@ -26,6 +30,7 @@ export default class ControllerAgent {
             this.summaryTool.call().then(console.log),
             this.testCasesTool.call().then(console.log),
             this.changelogTool.call().then(console.log),
+            this.incidentTool.call('Library response time crossed 500ms threshold').then(console.log),
         ]);
     }
 }
