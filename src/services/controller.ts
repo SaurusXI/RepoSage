1dcc7da0bda669cf2a00e08765a6188b08f05342
Shantanu Verma <vermashantanu@hotmail.com>
2023-08-18 09:41:52 +0530

    feat: add changelog and summary tools

--- src/services/controller.ts
+++ src/services/controller.ts
@@ -1,4 +1,6 @@
 import DiffsKnowledgeRepository from './knowledge/diffs.js';
+import ChangelogTool from './tools/changelog.js';
+import SummaryTool from './tools/summary.js';
 import TestCasesTool from './tools/tc.js';
 
 export default class ControllerAgent {
@@ -6,15 +8,24 @@ export default class ControllerAgent {
 
     private testCasesTool: TestCasesTool;
 
+    private summaryTool: SummaryTool;
+
+    private changelogTool: ChangelogTool;
+
     constructor() {
         this.diffsRepo = new DiffsKnowledgeRepository();
         this.testCasesTool = new TestCasesTool(this.diffsRepo);
+        this.summaryTool = new SummaryTool(this.diffsRepo);
+        this.changelogTool = new ChangelogTool(this.diffsRepo);
     }
 
     async processPR(prURL: string) {
         await this.diffsRepo.init(prURL);
 
-        await this.testCasesTool.call();
-        console.log(this.testCasesTool.testCases);
+        await Promise.all([
+            this.summaryTool.call().then(console.log),
+            this.testCasesTool.call().then(console.log),
+            this.changelogTool.call().then(console.log),
+        ]);
     }
 }
